8b4c41c1fb764cb74073731dec72ebc2fa55123d
Andrew Park <apark999@gmail.com>
2021-02-14 00:12:21 -0800

    unexpected token issue resolved

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
-
-
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, function() {
   console.log(`App listening on PORT ${PORT}`);
