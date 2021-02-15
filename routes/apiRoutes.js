192b0b7c223f4558828f841fac5178d4908bb9ea
Andrew Park <apark999@gmail.com>
2021-02-14 16:10:46 -0800

    emptied db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,17 +12,18 @@ module.exports = function(app) {
     res.json(notesObj[req.params.id]);
   });
 
-  //
+  // Adds new note to json file
   app.post('/api/notes', function(req, res) {
 
     let newNote = req.body;
     if (notesObj.length === 0) newNote.id = '0';
     else {
+      // assign new note the id plus one the highest existing id
       newNote.id = (parseInt(notesObj[notesObj.length - 1].id) + 1).toString();
     }
     notesObj.push(newNote);
-    console.log(`Created note with id ${newNote.id}`);
-        
+    //console.log(`Created note with id ${newNote.id}`);
+    // update json file
     fs.writeFileSync("./db/db.json", JSON.stringify(notesObj), (err) => {
       if (err) throw (err);    
     }); 
@@ -31,11 +32,12 @@ module.exports = function(app) {
 
   // delete a saved note by id
   app.delete('/api/notes/:id', function(req, res) {
-
+    // return note json minus the specific note
     notesObj = notesObj.filter(notes => {
       return notes.id != req.params.id;
     });
-    console.log(`Deleted note with id ${req.params.id}`); 
+    //console.log(`Deleted note with id ${req.params.id}`);
+    // update json file
     fs.writeFileSync('./db/db.json', JSON.stringify(notesObj), (err) => {
       if (err) throw err;
     });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,9 +2,11 @@ const path = require('path');
 
 module.exports = function(app) {
 
+  // display notes.html when /notes is accessed
   app.get('/notes', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
+  // display index.html when any other route is accessed
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
